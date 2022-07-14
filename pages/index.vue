a5122bb12b542ee05ee4a6b4d94007a6869e9653
www <www@gmail.com>
2022-07-14 19:09:51 +0800

    chore: init

---
+++ pages/index.vue
@@ -0,0 +1,17 @@
+<template>
+  <Tutorial />
+</template>
+
+<script setup lang="ts">
+import { useFetch,useAsync } from "@nuxtjs/composition-api"
+
+useAsync(()=>{
+  console.log(123);
+
+})
+
+useFetch(() => {
+  console.log(456);
+
+})
+</script>
